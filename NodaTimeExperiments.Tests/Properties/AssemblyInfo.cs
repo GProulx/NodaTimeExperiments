1d0242b3427f63b90f67c6d555fc70c125d0fb08
Ghislain Proulx <gproulx@ghislainproulx.net>
2017-12-28 13:51:35 -0500

    Initial tests

---
+++ NodaTimeExperiments.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NodaTimeExperiments.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NodaTimeExperiments.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74c10e9a-45e6-42f5-a4f9-5fab58e1ce9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NodaTimeExperiments.Tests/UnitTest1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NodaTime;
+using NodaTime.Extensions;
+using NodaTime.TimeZones;
+
+namespace NodaTimeExperiments.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Basic_FromWebSite()
+        {
+            // Instant represents time from epoch
+            Instant now = SystemClock.Instance.GetCurrentInstant();
+
+            // Convert an instant to a ZonedDateTime
+            ZonedDateTime nowInIsoUtc = now.InUtc();
+
+            // Create a duration
+            Duration duration = Duration.FromMinutes(3);
+
+            // Add it to our ZonedDateTime
+            ZonedDateTime thenInIsoUtc = nowInIsoUtc + duration;
+
+            // Time zone support (multiple providers)
+            var london = DateTimeZoneProviders.Tzdb["Europe/London"];
+
+            // Time zone conversions
+            var localDate = new LocalDateTime(2012, 3, 27, 0, 45, 00);
+            var before = london.AtStrictly(localDate);
+        }
+
+
+        [TestMethod]
+        public void Get_CurrentTime()
+        {
+            //// Instant represents time from epoch
+            //Instant now = SystemClock.Instance.GetCurrentInstant();
+
+            //// Time zone support (multiple providers)
+            //var qc = DateTimeZoneProviders.Tzdb["America/Toronto"];
+
+            //var nowLocal = qc.AtStrictly(now.InUtc());
+
+            //DateTimeZone zone = DateTimeZoneProviders.Tzdb["Europe/London"];
+            //ZonedClock clock = SystemClock.Instance.InZone(zone);
+            //LocalDate today = clock.GetCurrentLocalDateTime().Date;
+
+
+        }
+
+        [TestMethod]
+        public void Get_CurrentTimeQc()
+        {
+            // ARRANGE
+            var qcZone = DateTimeZoneProviders.Tzdb["America/Toronto"];
+
+            ZonedClock clock = SystemClock.Instance.InZone(qcZone);
+
+            // ACT
+            var now = clock.GetCurrentLocalDateTime();
+
+            // ASSERT
+            var expectedDateTime = DateTime.Now;
+            now.Hour.Should().Be(expectedDateTime.Hour);
+            now.Minute.Should().Be(expectedDateTime.Minute);
+            now.Second.Should().Be(expectedDateTime.Second);
+            now.Day.Should().Be(expectedDateTime.Day);
+        }
+
+
+        [TestMethod]
+        public void HowToSaveDateTimeToDB()
+        {
+            // ARRANGE
+            const string localZoneId = "America/Toronto";
+            var qcZone = DateTimeZoneProviders.Tzdb[localZoneId];
+
+            ZonedClock clock = SystemClock.Instance.InZone(qcZone);
+
+            // ACT
+            LocalDateTime now = clock.GetCurrentLocalDateTime();
+
+            var x = qcZone.AtStrictly(now);
+
+            // ASSERT
+            x.Zone.Id.Should().Be(localZoneId);
+
+            // What I need to save into the database:
+            Console.WriteLine(x.Zone.Id);
+            Console.WriteLine(x.ToInstant());
+            // BASED ON : https://groups.google.com/forum/#!topic/noda-time/cfzbyYXfRyI
+        }
+
+
+
+        [TestMethod]
+        public void ListAllTimezones()
+        {
+            TzdbDateTimeZoneSource.Default
+                .ZoneLocations
+                .ToList()
+                .ForEach(tz =>
+                {
+                    Console.WriteLine("ZoneId:{0} | CountryName:{1} | Comment:{2}", tz.ZoneId, tz.CountryName, tz.Comment);
+                });
+        }
+    }
+}
